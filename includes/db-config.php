60a11f2933bd67ccf2711f0f8e50501f120d74a1
Brian Harrison <brharr@microsoft.com>
2017-09-25 16:18:07 -0400

    Made changes to support DB App Settings in App Services

--- includes/db-config.php
+++ includes/db-config.php
@@ -1,16 +1,16 @@
 <?php
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for TODO App */
-define('DB_NAME', 'todos');
+define(getenv('DB_NAME'), 'todos');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define(getenv('DB_USER'), 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define(getenv('DB_PASSWORD'), 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define(getenv('DB_HOST'), 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
